4aedb8df393657084f7cfbf793f20706a251eb50
bromix <Matthias.Bromisch@gmail.com>
2021-12-01 19:04:07 +0100

    feat(String): provide copy and move operations.

--- Borg/include/Borg/String.h
+++ Borg/include/Borg/String.h
@@ -7,5 +7,13 @@ namespace Borg
     public:
         String();
         String(std::nullptr_t);
+
+        // Copy constructor and assignment operator.
+        String(const String &input);
+        String operator=(const String &input);
+
+        // Move constructor and assignment operator.
+        String(String &&input);
+        String operator=(String &&input);
     };
 }
--- Borg/src/String.cpp
+++ Borg/src/String.cpp
@@ -1,8 +1,31 @@
 #include "Borg/String.h"
+#include <stdexcept>
 
 namespace Borg
 {
     String::String() {}
 
-    String::String(std::nullptr_t){}
+    String::String(std::nullptr_t) {}
+
+    // Copy constructor and assignment operator.
+    String::String(const String &input)
+    {
+        throw std::exception("Not implemented");
+    }
+
+    String String::operator=(const String &input)
+    {
+        throw std::exception("Not implemented");
+    }
+
+    // Move constructor and assignment operator.
+    String::String(String &&input)
+    {
+        throw std::exception("Not implemented");
+    }
+
+    String String::operator=(String &&input)
+    {
+        throw std::exception("Not implemented");
+    }
 }
