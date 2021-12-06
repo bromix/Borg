9000eeb7d2c94c32facb33b8b471ceb566e5955c
bromix <Matthias.Bromisch@gmail.com>
2021-12-06 18:13:45 +0100

    cleanup(Types): added some comments

--- Borg/include/Borg/Types.h
+++ Borg/include/Borg/Types.h
@@ -14,10 +14,21 @@ namespace Borg
         return std::make_shared<T>(std::forward<Args>(args)...);
     }
 
-    template<typename Result, typename... Args>
+    /**
+     * @brief Encapsulates a method that has zero or more Args and returns a value of the type specified by the Result parameter.
+     * https://docs.microsoft.com/en-us/dotnet/api/system.func-1
+     * @tparam Result
+     * @tparam Args
+     */
+    template <typename Result, typename... Args>
     using Func = std::function<Result(Args &&...args)>;
-    
-    template<typename... Args>
+
+    /**
+     * @brief Encapsulates a method that has zero or more Args and does not return a value.
+     * https://docs.microsoft.com/en-us/dotnet/api/system.action
+     * @tparam Args
+     */
+    template <typename... Args>
     using Action = std::function<void(Args &&...args)>;
 
     using uint8 = uint8_t;
