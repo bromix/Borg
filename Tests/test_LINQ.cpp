b72951445b9a6c3f25ee209a821ef77ed197b958
bromix <Matthias.Bromisch@gmail.com>
2021-12-17 01:30:10 +0100

    select test

--- Tests/test_LINQ.cpp
+++ Tests/test_LINQ.cpp
@@ -4,9 +4,10 @@
 
 using namespace Borg;
 
-struct Person
+class Person
 {
-    Person(const std::string& name, int age)
+public:
+    Person(const std::string &name, int age)
     {
         this->name = name;
         this->age = age;
@@ -31,10 +32,33 @@ TEST(LINQ, struct)
 {
     std::vector<Ref<Person>> persons = {
         CreateRef<Person>("Hans", 35),
-        CreateRef<Person>("Peter", 24)
-    };
+        CreateRef<Person>("Peter", 24)};
     auto result = LINQ::From(persons)
                       .Where([](Ref<Person> x) -> bool
                              { return x->name == "Hans"; })
                       .ToVector();
+}
+
+template <typename Source, typename Target>
+std::vector<Target> Select(const std::vector<Source> &input, Func<Target, Source> func)
+{
+    std::vector<Target> result;
+    
+    for (Source x : input)
+    {
+        Target t = func(std::move(x));
+        result.push_back(t);
+    }
+        
+
+    return result;
+}
+
+TEST(LINQ, select)
+{
+    std::vector<Ref<Person>> persons = {
+        CreateRef<Person>("Hans", 35),
+        CreateRef<Person>("Peter", 24)};
+    auto result = Select<Ref<Person>, std::string>(persons, [](Ref<Person> x) -> std::string
+                                                   { return x->name; });
 }
