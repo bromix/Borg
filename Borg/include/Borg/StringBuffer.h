87d27024e050de7a0d6f5813c053b29382cb1f00
bromix <Matthias.Bromisch@gmail.com>
2021-12-30 11:58:18 +0100

    chore: cleanup Buffer

--- Borg/include/Borg/StringBuffer.h
+++ Borg/include/Borg/StringBuffer.h
@@ -8,7 +8,7 @@ namespace Borg
     class StringBuffer : public Buffer<T>
     {
     public:
-        StringBuffer(nullptr_t) : Buffer<T>(nullptr) {}
+        StringBuffer(nullptr_t) : Buffer<T>(nullptr){};
 
         StringBuffer(std::basic_string_view<T> input) : StringBuffer(input.length())
         {
@@ -49,12 +49,15 @@ namespace Borg
         {
             return std::basic_string<T>(m_Data);
         }
+
     };
 
     using CharBuffer = StringBuffer<char>;
     using WideCharBuffer = StringBuffer<wchar_t>;
 #ifdef __cpp_lib_char8_t
     using Char8Buffer = StringBuffer<char8_t>;
+#else
+    using Char8Buffer = StringBuffer<char>;
 #endif // __cpp_lib_char8_t
     using Char16Buffer = StringBuffer<char16_t>;
     using Char32Buffer = StringBuffer<char32_t>;
--- BorgTests/test_Buffer.cpp
+++ BorgTests/test_Buffer.cpp
@@ -1,13 +1,57 @@
 #include "Borg/Borg.h"
 #include "Borg/Buffer.h"
+#include "Borg/StringBuffer.h"
 #include <gtest/gtest.h>
 
 using namespace Borg;
 
-TEST(MemoryBlock, Ctr)
+TEST(Buffer, SizeTest)
 {
-    auto size = sizeof(wchar_t);
-    Buffer<wchar_t> mb = {10};
-    ASSERT_EQ(10 * size, mb.Size());
-    ASSERT_EQ(10, mb.Count());
+    auto count = 10;
+    auto size = sizeof(wchar_t) * count;
+    Buffer<wchar_t> mb(count);
+    ASSERT_EQ(size, mb.Size());
+    ASSERT_EQ(count, mb.Count());
+}
+
+TEST(Buffer, Null)
+{
+    Buffer<wchar_t> mb = nullptr;
+    ASSERT_TRUE(mb.IsNull());
+    ASSERT_TRUE(mb.IsNullOrEmpty());
+}
+
+TEST(Buffer, Size0)
+{
+    Buffer<wchar_t> mb((std::size_t)0);
+    ASSERT_TRUE(mb.IsEmpty());
+    ASSERT_EQ(0, mb.Size());
+    ASSERT_EQ(0, mb.Count());
+}
+
+TEST(CharBuffer, Size)
+{
+    auto count = 10;
+    auto size = sizeof(char) * (count + 1); // +1 because of null-termination
+    CharBuffer cb(count);
+    ASSERT_EQ(count, cb.Count());
+    ASSERT_EQ(size, cb.Size());
+}
+
+TEST(Char16Buffer, Size)
+{
+    auto count = 10;
+    auto size = sizeof(char16_t) * (count + 1); // +1 because of null-termination
+    Char16Buffer cb(count);
+    ASSERT_EQ(count, cb.Count());
+    ASSERT_EQ(size, cb.Size());
+}
+
+TEST(Char32Buffer, Size)
+{
+    auto count = 10;
+    auto size = sizeof(char32_t) * (count + 1); // +1 because of null-termination
+    Char32Buffer cb(count);
+    ASSERT_EQ(count, cb.Count());
+    ASSERT_EQ(size, cb.Size());
 }
