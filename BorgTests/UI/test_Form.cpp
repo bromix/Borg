d8ddb38d6923b1c1755759662b1200212af10910
bromix <Matthias.Bromisch@gmail.com>
2022-01-13 19:15:44 +0100

    test(Form): added Test for development.

---
+++ BorgTests/UI/test_Form.cpp
@@ -0,0 +1,10 @@
+#include "Borg/UI/Form.h"
+#include <gtest/gtest.h>
+
+using namespace Borg;
+
+TEST(UIForm, DebugTest)
+{
+    Ref<UI::IForm> form = CreateRef<UI::Form>();
+    form->ShowDialog();
+}
