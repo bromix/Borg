5174425b6e8ee3a26b769592b1551bbe91cf3db5
bromix <Matthias.Bromisch@gmail.com>
2022-01-15 18:44:20 +0100

    refactor: moved WndProc into own header.

--- Borg/src/Platform/Windows/UI/Form.cpp
+++ Borg/src/Platform/Windows/UI/Form.cpp
@@ -1,6 +1,7 @@
 #include "Borg/UI/Form.h"
 #include "Borg/Exception.h"
 #include "Borg/Types.h"
+#include "WndProc.h"
 
 namespace Borg::UI
 {
@@ -26,41 +27,6 @@ namespace Borg::UI
     constexpr const long BORG_FORMBORDERSTYLE_NONE_EXSTYLE = 0;
 
     constexpr const wchar_t *BORG_UI_FORM_CLASSNAME = L"Borg::UI::Form";
-
-    class WndProxy
-    {
-    public:
-        WndProxy(const Func<LRESULT, const UI::Message &> &wndProc) : m_wndProc(wndProc)
-        {
-        }
-
-        LRESULT operator()(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
-        {
-            return m_wndProc({hWnd, message, wParam, lParam});
-        }
-
-    private:
-        Func<LRESULT, const UI::Message &> m_wndProc;
-    };
-
-    LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
-    {
-        WndProxy *wndProxy = nullptr;
-
-        if (message == WM_NCCREATE)
-        {
-            LPCREATESTRUCT lpcs = reinterpret_cast<LPCREATESTRUCT>(lParam);
-            wndProxy = static_cast<WndProxy *>(lpcs->lpCreateParams);
-            ::SetWindowLongPtr(hWnd, GWLP_USERDATA, reinterpret_cast<LONG_PTR>(wndProxy));
-        }
-        else
-            wndProxy = reinterpret_cast<WndProxy *>(::GetWindowLongPtr(hWnd, GWLP_USERDATA));
-
-        if (wndProxy)
-            return (*wndProxy)(hWnd, message, wParam, lParam);
-
-        return DefWindowProcW(hWnd, message, wParam, lParam);
-    }
 
     void registerForm(HINSTANCE hInst)
     {
