a82f3e37df7baa1420fdecb1609c14dd2cbc0ad7
bromix <Matthias.Bromisch@gmail.com>
2022-01-16 17:29:09 +0100

    fix(CustromControl): we need a parent Handle

--- Borg/src/Platform/Windows/UI/CustomControl.cpp
+++ Borg/src/Platform/Windows/UI/CustomControl.cpp
@@ -3,20 +3,19 @@
 
 namespace Borg::UI
 {
-    CustomControl::CustomControl(const String &className)
+    CustomControl::CustomControl(const UI::IControl& parent, const String &className)
     {
-        //UI::Handle hOwner = owner ? owner->Handle() : nullptr;
-        UI::Handle hOwner = nullptr;
+        UI::Handle hParent = parent.Handle() ? parent.Handle() : nullptr;
 
         CREATESTRUCTW cs{0};
         cs.lpszClass = Encoding::Convert<WideCharBuffer>(className);
-        cs.style = CS_HREDRAW | CS_VREDRAW | WS_OVERLAPPED;
+        cs.style = CS_DBLCLKS | WS_CHILD | WS_CLIPCHILDREN | WS_CLIPSIBLINGS;
         cs.dwExStyle = WS_EX_LEFT;
-        cs.hwndParent = hOwner;
-        cs.x = CW_USEDEFAULT;
-        cs.y = CW_USEDEFAULT;
-        cs.cx = CW_USEDEFAULT;
-        cs.cy = CW_USEDEFAULT;
+        cs.hwndParent = hParent;
+        cs.x = 50;
+        cs.y = 50;
+        cs.cx = 100;
+        cs.cy = 100;
         m_Handle = ClassFactory::Create(cs, [this](const UI::Message &message)
                                         { return this->onMessage(message); });
     }
