346c45f74e78777d713cdf0ff8361c1194ac788f
bromix <matthias.bromisch@gmail.com>
2022-01-17 14:24:24 +0100

    cleanup: fixed some namespaces

--- Borg/include/Borg/UI/CustomControl.h
+++ Borg/include/Borg/UI/CustomControl.h
@@ -6,6 +6,6 @@ namespace Borg::UI
     class CustomControl : public virtual UI::Control
     {
     public:
-        CustomControl(const Ref<UI::IControl> &parent, const String &className);
+        CustomControl(const Borg::WeakRef<UI::IControl> &parent, const String &className);
     };
 }
--- Borg/src/Platform/Windows/UI/CustomControl.cpp
+++ Borg/src/Platform/Windows/UI/CustomControl.cpp
@@ -3,7 +3,7 @@
 
 namespace Borg::UI
 {
-    CustomControl::CustomControl(const Ref<UI::IControl> &parent, const String &className) : UI::Control(parent)
+    CustomControl::CustomControl(const WeakRef<UI::IControl> &parent, const String &className) : UI::Control(parent)
     {
         UI::Handle hParent = UI::Handle::GetSafeFrom(m_InternalParent.lock());
 
