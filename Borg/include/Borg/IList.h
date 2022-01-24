7098a705297beed943f80f307d84d69393edefd5
bromix <Matthias.Bromisch@gmail.com>
2022-01-24 20:48:34 +0100

    feat(List): moved iterator begin() and end() to interface.

--- Borg/include/Borg/IList.h
+++ Borg/include/Borg/IList.h
@@ -1,13 +1,30 @@
 #pragma once
+#include <vector>
 
 namespace Borg
 {
-    template<typename T>
+    template <typename T>
     class IList2
     {
     public:
+        using Iterator = typename std::vector<T>::iterator;
+
         virtual ~IList2() = default;
 
         virtual void Add(const T &item) = 0;
+
+        Iterator begin() noexcept
+        {
+            return doBegin();
+        }
+
+        Iterator end() noexcept
+        {
+            return doEnd();
+        }
+
+    protected:
+        virtual Iterator doBegin() = 0;
+        virtual Iterator doEnd() = 0;
     };
 }
--- Borg/include/Borg/List.h
+++ Borg/include/Borg/List.h
@@ -4,33 +4,31 @@
 
 namespace Borg
 {
-    template<typename T>
-    class IList: public std::vector<T>
+    template <typename T>
+    class IList : public std::vector<T>
     {
     };
 
-    template<typename T>
-    class List: public IList<T>
+    template <typename T>
+    class List : public IList<T>
     {
     };
 
-    template<typename T>
-    class List2: public virtual IList2<T>
+    template <typename T>
+    class List2 : public virtual IList2<T>
     {
     public:
-        using Iterator = typename std::vector<T>::iterator;
-
         void Add(const T &item) override
         {
             m_List.push_back(item);
         }
-
-        Iterator begin() noexcept
+    protected:
+        Iterator doBegin() override
         {
             return m_List.begin();
         }
-
-        Iterator end() noexcept
+        
+        Iterator doEnd() override
         {
             return m_List.end();
         }
