8d328fe2375a649277f8642598bea4114aa54a44
bromix <matthias.bromisch@gmail.com>
2022-08-09 12:14:48 +0200

    cleanup

--- Borg/include/Borg/UI/Form.h
+++ Borg/include/Borg/UI/Form.h
@@ -1,38 +1,60 @@
 #pragma once
+
 #include "IForm.h"
 #include "Control.h"
 
 namespace Borg::UI
 {
-    class Form : public Control, public IForm
-    {
-    public:
-        ~Form();
-        Form();
-        Form(const Ref<UI::IForm> &owner);
-        Ref<IForm> GetOwner() const override;
-        void SetOpacity(double opacity) override;
-        double GetOpacity() const override;
-        void SetFormBorderStyle(FormBorderStyle style) override;
-        UI::FormBorderStyle GetFormBorderStyle() const override;
-        void CenterToParent() override;
-        void CenterToScreen() override;
-        void SetShowInTaskbar(bool show) override;
-        bool GetShowInTaskbar() const override;
-        void Close() override;
-        void Show() override;
-        UI::DialogResult ShowDialog() override;
-        void SetDialogResult(UI::DialogResult dialogResult) override;
-        UI::DialogResult GetDialogResult() const override;
+	class Form : public Control, public IForm
+	{
+	public:
+		~Form() override;
+
+		Form();
+
+		explicit Form(const Ref<UI::IForm>& owner);
+
+		Ref<IForm> GetOwner() const override;
+
+		void SetOpacity(double opacity) override;
+
+		double GetOpacity() const override;
+
+		void SetFormBorderStyle(FormBorderStyle style) override;
+
+		UI::FormBorderStyle GetFormBorderStyle() const override;
+
+		void CenterToParent() override;
+
+		void CenterToScreen() override;
+
+		void SetShowInTaskbar(bool show) override;
+
+		bool GetShowInTaskbar() const override;
+
+		void Close() override;
+
+		void Show() override;
+
+		UI::DialogResult ShowDialog() override;
+
+		void SetDialogResult(UI::DialogResult dialogResult) override;
+
+		UI::DialogResult GetDialogResult() const override;
+
 #ifdef _WIN32
-        void DragMove() override;
+
+		void DragMove() override;
+
 #endif
 
-        static Ref<UI::IForm> CreateFrom(const UI::Handle &handle);
+		static Ref<UI::IForm> CreateFrom(const UI::Handle& handle);
 
-    protected:
-        UI::DialogResult m_DialogResult{DialogResult::None};
-        void onClosed(const UI::FormClosedEventArgs &e) override;
-        void onMessage(UI::Message &message) override;
-    };
+	protected:
+		UI::DialogResult m_DialogResult{ DialogResult::None };
+
+		void onClosed(const UI::FormClosedEventArgs& e) override;
+
+		void onMessage(UI::Message& message) override;
+	};
 }
